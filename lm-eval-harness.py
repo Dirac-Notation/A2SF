23f274223997c169386a26a6390ecbe738111545
Dirac-Notation <smp9898@naver.com>
2024-05-20 10:35:40 +0900

    24-05-20

--- lm-eval-harness.py
+++ lm-eval-harness.py
@@ -18,13 +18,13 @@ def lm_test(lm_model: huggingface.HFLM, task_dict: dict):
 # load task dataset
 initialize_tasks()
 task_list = ["openbookqa", "winogrande", "piqa", "copa", "mathqa", "arc_easy", "arc_challenge"]
-# task_list = ["mmlu"]
+task_list = ["mmlu"]
 model_list = ["meta-llama/Llama-2-7b-hf", "huggyllama/llama-7b", "facebook/opt-6.7b", "facebook/opt-2.7b"]
 fewshot_list = [1, 0]
-# ratio_list = [0.1, 0.2]
+ratio_list = [0.1, 0.2]
 # ratio_list = [0.3, 0.4, 0.5]
 # ratio_list = [0.6, 0.8]
-ratio_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+# ratio_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
 
 for model_name in model_list:
@@ -47,23 +47,23 @@ for model_name in model_list:
         for ratio in ratio_list:
             print(f"================={ratio}=================")
 
-            # # local
-            # task = tasks.get_task_dict(task_list, num_fewshot=num_fewshot, fewshot_split="validation")
-            # lm_model(model_name=model_name, lm=lm, check_point=check_point, heavy_ratio=0.0, recent_ratio=ratio, penalty=1.0)
-            # lm.model.eval().half().cuda()
-            # print("Local")
-            # lm_test(lm, task)
+            # local
+            task = tasks.get_task_dict(task_list, num_fewshot=num_fewshot, fewshot_split="validation")
+            lm_model(model_name=model_name, lm=lm, check_point=check_point, heavy_ratio=0.0, recent_ratio=ratio, penalty=1.0)
+            lm.model.eval().half().cuda()
+            print("Local")
+            lm_test(lm, task)
 
-            # # h2o
-            # task = tasks.get_task_dict(task_list, num_fewshot=num_fewshot, fewshot_split="validation")
-            # lm_model(model_name=model_name, lm=lm, check_point=check_point, heavy_ratio=ratio/2, recent_ratio=ratio/2, penalty=1.0)
-            # lm.model.eval().half().cuda()
-            # print("H2O")
-            # lm_test(lm, task)
+            # h2o
+            task = tasks.get_task_dict(task_list, num_fewshot=num_fewshot, fewshot_split="validation")
+            lm_model(model_name=model_name, lm=lm, check_point=check_point, heavy_ratio=ratio/2, recent_ratio=ratio/2, penalty=1.0)
+            lm.model.eval().half().cuda()
+            print("H2O")
+            lm_test(lm, task)
 
             # decay
             task = tasks.get_task_dict(task_list, num_fewshot=num_fewshot, fewshot_split="validation")
-            lm_model(model_name=model_name, lm=lm, check_point=check_point, heavy_ratio=0.3, recent_ratio=0.0, penalty=ratio)
+            lm_model(model_name=model_name, lm=lm, check_point=check_point, heavy_ratio=ratio, recent_ratio=0.0, penalty=0.1)
             lm.model.eval().half().cuda()
             print("H2O-decay")
             lm_test(lm, task)
