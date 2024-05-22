3822d9d2f476842bbae3d028fafcc814b95b8e21
Dirac-Notation <smp9898@naver.com>
2024-05-23 04:57:48 +0900

    24-05-23

---
+++ lm-eval-harness.py
@@ -0,0 +1,68 @@
+import copy
+import torch
+
+from lm_eval import tasks, evaluator
+from lm_eval.models import huggingface
+from lm_eval.tasks import initialize_tasks
+
+from utils_lm_eval.lm_model import lm_model
+
+def lm_test(lm_model: huggingface.HFLM, task_dict: dict):
+    results = evaluator.evaluate(lm_model, task_dict)
+
+    print(evaluator.make_table(results))
+    if "groups" in results:
+        print(evaluator.make_table(results, "groups"))
+    print("==============================================")
+
+# load task dataset
+initialize_tasks()
+task_list = ["openbookqa", "winogrande", "piqa", "copa", "mathqa", "arc_easy", "arc_challenge"]
+model_list = ["meta-llama/Llama-2-7b-hf", "huggyllama/llama-7b", "facebook/opt-6.7b", "facebook/opt-2.7b"]
+fewshot_list = [1, 0]
+ratio_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8]
+
+
+for model_name in model_list:
+    print(f"model: {model_name}")
+    # Load the model
+    lm = huggingface.HFLM(model_name, batch_size="2")
+    
+    lm.model.eval().half().cpu()
+    check_point = copy.deepcopy(lm.model.state_dict())
+    lm.model.cuda()
+
+    for num_fewshot in fewshot_list:
+        print(f"fewshot: {num_fewshot}")
+        
+        # Full Result
+        task = tasks.get_task_dict(task_list, num_fewshot=num_fewshot, fewshot_split="validation")
+        print("Full")
+        lm_test(lm, task)
+
+        for ratio in ratio_list:
+            print(f"================={ratio}=================")
+
+            # local
+            task = tasks.get_task_dict(task_list, num_fewshot=num_fewshot, fewshot_split="validation")
+            lm_model(model_name=model_name, lm=lm, check_point=check_point, heavy_ratio=0.0, recent_ratio=ratio, penalty=1.0)
+            lm.model.eval().half().cuda()
+            print("Local")
+            lm_test(lm, task)
+
+            # h2o
+            task = tasks.get_task_dict(task_list, num_fewshot=num_fewshot, fewshot_split="validation")
+            lm_model(model_name=model_name, lm=lm, check_point=check_point, heavy_ratio=ratio/2, recent_ratio=ratio/2, penalty=1.0)
+            lm.model.eval().half().cuda()
+            print("H2O")
+            lm_test(lm, task)
+
+            # decay
+            task = tasks.get_task_dict(task_list, num_fewshot=num_fewshot, fewshot_split="validation")
+            lm_model(model_name=model_name, lm=lm, check_point=check_point, heavy_ratio=ratio, recent_ratio=0.0, penalty=0.0)
+            lm.model.eval().half().cuda()
+            print("H2O-decay")
+            lm_test(lm, task)
+
+    del lm
+    torch.cuda.empty_cache()
---
+++ utils_lm_eval/lm_model.py
@@ -0,0 +1,34 @@
+import copy
+import torch
+
+from typing import Union
+
+from transformers import AutoModelForCausalLM, AutoConfig
+
+from utils_lm_eval.modify_llama import convert_kvcache_llama_heavy_recent
+from utils_lm_eval.modify_opt import convert_kvcache_opt_heavy_recent
+from lm_eval.models.huggingface import HFLM
+
+ENABLE_Heavy_Hitter_FUNCTIONS = {
+    "OPTForCausalLM": convert_kvcache_opt_heavy_recent,
+    "LlamaForCausalLM": convert_kvcache_llama_heavy_recent
+}
+
+def lm_model(model_name: str,
+             lm: HFLM=None,
+             check_point=None,
+             heavy_ratio: float = 0.1,
+             recent_ratio: float = 0.1,
+             penalty: float = 1.0,
+            ):
+    config = AutoConfig.from_pretrained(model_name)
+    config.heavy_ratio = heavy_ratio
+    config.recent_ratio = recent_ratio
+    config.penalty = penalty
+
+    lm.model.cpu()
+
+    ENABLE_Heavy_Hitter_FUNCTIONS[config.architectures[0]](lm.model, config=config)
+
+    torch.cuda.empty_cache()
+    lm.model.load_state_dict(check_point)
