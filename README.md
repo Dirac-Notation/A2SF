358dbf14d6a6a645830c2f1940ade2f5e27a4ba4
Dirac-Notation <smp9898@naver.com>
2024-05-25 11:54:01 +0900

    24-05-25

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # A2SF
 
 Accumulative Attention Score with Forgetting Factor
+
+## Text Generation
+
+sh scripts/generation.sh model_arch model_name cache_ratio penalty
+
+default: llama meta-llama/Llama-2-7b-hf 0.3 0.1
--- scripts/generation/text_generation.sh
+++ scripts/generation/text_generation.sh
@@ -1,4 +1,10 @@
-python -u run_text_generation.py \
-    --model_arch llama \
-    --model_name meta-llama/Llama-2-7b-hf \
-    --cache_ratio 0.3 --penalty 0.2
+#!/bin/bash
+
+model_arch=${1:-llama}
+model_name=${2:-meta-llama/Llama-2-7b-hf}
+cache_ratio=${3:-0.3}
+penalty=${4:-0.2}
+
+python -u run_text_generation.py -model_arch $model_arch \
+    --model_name $model_name \
+    --cache_ratio $cache_ratio --penalty $penalty
