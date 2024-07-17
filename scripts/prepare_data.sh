c0ac898340ddcd88c1a0182ab96e233ab5730d92
Dirac-Notation <smp9898@naver.com>
2024-07-17 20:53:09 +0900

    24-07-17

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -2,22 +2,22 @@
 
 shots=$1
 
-python -u generate_task_data.py --output-file lm/openbookqa-${shots}.jsonl \
+python -u generate_task_data.py --output-file data/openbookqa-${shots}.jsonl \
     --task-name openbookqa \
     --num-fewshot ${shots}
 
-python -u generate_task_data.py --output-file lm/piqa-${shots}.jsonl \
+python -u generate_task_data.py --output-file data/piqa-${shots}.jsonl \
     --task-name piqa \
     --num-fewshot ${shots}
 
-python -u generate_task_data.py --output-file lm/arc_challenge-${shots}.jsonl \
+python -u generate_task_data.py --output-file data/arc_challenge-${shots}.jsonl \
     --task-name arc_challenge \
     --num-fewshot ${shots}
 
-python -u generate_task_data.py --output-file lm/arc_easy-${shots}.jsonl \
+python -u generate_task_data.py --output-file data/arc_easy-${shots}.jsonl \
     --task-name arc_easy \
     --num-fewshot ${shots}
 
-python -u generate_task_data.py --output-file lm/mathqa-${shots}.jsonl \
+python -u generate_task_data.py --output-file data/mathqa-${shots}.jsonl \
     --task-name mathqa \
     --num-fewshot ${shots}
